d73ae4ccc8e88f77d42d3c75874792fc023ccffd
GoutamPatidar <goutampatidar05@gmail.com>
2024-06-03 17:08:38 +0530

    "Hooksi react "

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+//hooks are used to propogate changes to ui 
+
+  let [counter, setCounter] = useState(15)
+  // let counter=15;
+  const addValue = () => {
+    //counter++;
+    setCounter(counter+1)
+    console.log(counter, " clicked");
+
+  }
+
+  const removevalue=()=>{
+    setCounter(counter-1)
+    console.log(counter, " clicked");
+  }
+  return (
+    <>
+      <h1>Goutam Patidar</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>Add value {counter}</button><br /><button onClick={removevalue}>Remove value {counter}</button>
+      <footer>footer: {counter}</footer>
+    </>
+  )
+}
+
+export default App
